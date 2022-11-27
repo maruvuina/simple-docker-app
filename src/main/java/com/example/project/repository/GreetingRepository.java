ba6e3dcd082c2c02193303024387945929bdbfa7
Ina Prymava <Ina_Prymava@epam.com>
2022-11-27 19:13:15 +0300

    changed to JPA repository

--- src/main/java/com/example/project/repository/GreetingRepository.java
+++ src/main/java/com/example/project/repository/GreetingRepository.java
@@ -1,7 +1,7 @@
 package com.example.project.repository;
 
 import com.example.project.model.Greeting;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GreetingRepository extends CrudRepository<Greeting, Long> {
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
 }
